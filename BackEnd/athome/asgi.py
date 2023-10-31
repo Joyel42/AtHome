d246cb3d713164f0172681beeacd14189a4a4fcb
Joyel <joyel.g@atai.ai>
2023-10-31 16:55:48 +0530

    Basic Structure

--- BackEnd/athome/asgi.py
+++ BackEnd/athome/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for atHome project.
+ASGI config for athome project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atHome.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'athome.settings')
 
 application = get_asgi_application()
